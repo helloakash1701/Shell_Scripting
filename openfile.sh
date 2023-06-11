b3be2b6893c0a757abb42624642c84471da8c18a
Akash Mishra <80618499+helloakash1701@users.noreply.github.com>
2023-06-12 02:45:49 +0530

    Added shell files

---
+++ openfile.sh
@@ -0,0 +1,15 @@
+
+
+#!/bin/bash
+
+echo "Creating folder"
+
+mkdir NewFolder
+
+cd NewFolder
+
+touch yourfile.txt
+
+
+
+
---
+++ passwordgenerator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Password Generator
+
+echo "Enter the length of the password"
+read pass_len
+
+for p in $(seq 1 5);
+do
+	openssl rand -base64 48 | cut -c1-$pass_len
+done
+
