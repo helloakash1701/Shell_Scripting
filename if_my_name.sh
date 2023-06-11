df8a5554984af9b2118dad80e7120d364a6e772d
Akash Mishra <mishrakash1701@gmail.com>
2023-06-12 02:43:24 +0530

    Added shell files

---
+++ if_my_name.sh
@@ -0,0 +1,21 @@
+
+
+
+#!/bin/bash
+
+#This will be an if-else script
+
+
+echo "Please enter your name"
+read name
+
+if [ name == "Akash" ]
+then
+echo "Welcome ${name}"
+else
+echo "Who are you ${name}"
+fi
+
+
+
+
