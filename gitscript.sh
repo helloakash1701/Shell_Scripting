aef315e3d731799fed19c139ae15b2e7408bd59f
Akash Mishra <mishrakash1701@gmail.com>
2023-06-12 13:54:01 +0530

    Added your files

--- gitscript.sh
+++ gitscript.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/env bash
 
 
 #Making a script which will automatically push your code to your repo
@@ -11,18 +11,18 @@ git commit -m "Added your files"
 
 echo "Please enter your github mail"
 
-read email
+read -r email
 
 echo "$email"
 
-echo "Please confirm your github id with "Y" or "N" "
+echo "Please confirm your github id with Y or N "
 
-read output
+read -r output
 
-if [ ${output} == "Y" ]
+if [ "${output}" == "Y" ]
 then
-git remote add origin $email
-git push origin master 
+git remote add origin "$email"
+git push origin master
 else
 echo " Couldn't find your mail"
 fi
