9fcd332e4583cbebecff6eaed84d24099fe5541d
Akash Mishra <mishrakash1701@gmail.com>
2023-06-12 13:43:03 +0530

    Added your files

--- gitscript.sh
+++ gitscript.sh
@@ -9,10 +9,14 @@ git add .
 
 git commit -m "Added your files"
 
+echo "Please enter your github mail"
+
+read email
+
 echo email
 
 echo "Please confirm your github id with "Y" or "N" "
-read output
+
 if [ output == "Y" ]
 then
 git remote add origin $email
